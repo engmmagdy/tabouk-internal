46d80973bd3c8fb34efe04fffc1cbcc38c6a1f8d
Mohamed Magdy <eng.mmagdy887@gmail.com>
2020-09-23 04:12:12 +0200

    new files

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,20 +2,22 @@ $(function () {
   
   //$('marquee li').trigger('mouseout');
 
-  $(".main-news .slider").slick({
-    slide: 'article',
-    slidesToShow: 1,
+  $(".services__slider").slick({
+    slidesToShow: 6,
     slidesToScroll: 1,
     slidesPerRow: 1,
     speed: 1200,
     rtl: true,
-    dots: true,
-    arrows: false,
+    dots: false,
+    arrows: true,
+    prevArrow: '<div class="prevArrow"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" viewBox="0 0 256 512"><path fill="" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/></svg></div>',
+    nextArrow: '<div class="nextArrow"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" viewBox="0 0 256 512"><path fill="" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg></div>',
     autoplay: false,
     autoplaySpeed: 5000,
     pauseOnHover: false
   });
 
+  
   
 });
 
