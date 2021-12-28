d4994ee9de929c07567c218438bc551e8a0dad10
Mhammed998 <mohashim998@gmail.com>
2021-12-28 21:54:07 +0200

    push updates

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,12 @@
+// show extended form
+let extendBtn = document.getElementById("extendButton");
+let extendedForm = document.getElementById("extendedForm");
+
+extendBtn.addEventListener("click", () => {
+  console.log("clicked");
+  extendedForm.style.cssText = "display: block ";
+});
+
 $(function () {
   // const activateEditTable = () => {
   //   const checked = $('#edit').is(":checked")
@@ -111,5 +120,46 @@ triggerTabList.forEach(function (triggerEl) {
   triggerEl.addEventListener("click", function (event) {
     event.preventDefault();
     tabTrigger.show();
+  });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $("#exampleModal").on("hidden.bs.modal", function (event) {
+    extendedForm.style.cssText = "display: none";
+  });
+
+  // drag && drop file
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $(".image-upload-wrap").hide();
+
+        $(".file-upload-image").attr("src", e.target.result);
+        $(".file-upload-content").show();
+
+        $(".image-title").html(input.files[0].name);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      removeUpload();
+    }
+  }
+
+  function removeUpload() {
+    $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+    $(".file-upload-content").hide();
+    $(".image-upload-wrap").show();
+  }
+  $(".image-upload-wrap").bind("dragover", function () {
+    $(".image-upload-wrap").addClass("image-dropping");
+  });
+  $(".image-upload-wrap").bind("dragleave", function () {
+    $(".image-upload-wrap").removeClass("image-dropping");
   });
 });
